c344c65b38d19298b606661cde20236c58bb8421
GlitchHunterCoder <TheoChalmers2008@gmail.com>
2026-01-16 21:42:57 +0000

    Update TaskPriority.js

--- modules/TaskPriority.js
+++ modules/TaskPriority.js
@@ -1,33 +1,33 @@
 PM.add("TaskPriority", {
   queueMicrotask(fn, ...params) {
     const gen = (function* () {
-      yield* TS.exe(fn, ...params)
+      yield* TS.run(fn, ...params)
     })()
     return TS.add(gen, 1)
   },
 
   nextTick(fn, ...params) {
     const gen = (function* () {
-      yield* TS.exe(fn, ...params)
+      yield* TS.run(fn, ...params)
     })()
     return TS.add(gen, 2)
   },
 
   override(fn, ...params) {
     const gen = (function* () {
-      yield* TS.exe(fn, ...params)
+      yield* TS.run(fn, ...params)
     })()
     return TS.add(gen, Infinity)
   },
 
   idle(fn, ...params) {
     const gen = (function* () {
-      yield* TS.exe(fn, ...params)
+      yield* TS.run(fn, ...params)
     })()
     return TS.add(gen, -Infinity)
   },
 
   await(fn, ...params) {
-    return TS.exe(fn, ...params)
+    return TS.run(fn, ...params)
   }
 })
