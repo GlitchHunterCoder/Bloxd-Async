89df4597d44d6d483f5edfd90fbe766d0c7a03b4
GlitchHunterCoder <TheoChalmers2008@gmail.com>
2026-01-27 14:15:23 +0000

    Update bloxd_env.js

--- bloxd_env.js
+++ bloxd_env.js
@@ -1,13 +1,5 @@
-let _T=function*(){
-  i=0
-  while(1){
-    console.log(`=== TICK ${i} ===`)
-    tick()
-    i++
-    yield;
-  }
-}()
-
-let T=()=>{_T.next()}
-
-setInterval(T,50)
+(()=>{
+  let _T=function*(){while(1){tick();yield}}()
+  let T=()=>{_T.next()}
+  setInterval(T,50)
+})()
