35fbe45e13f453b8d349c0e456be9cfee783b4c1
GlitchHunterCoder <209635473+GlitchHunterCoder@users.noreply.github.com>
2026-03-14 18:54:55 +0000

    Update AsyncTiming.js

--- modules/AsyncTiming.js
+++ modules/AsyncTiming.js
@@ -1,36 +1,26 @@
-PM.add("AsyncTiming", {
+PM.localAdd("AsyncTiming", {
   sleep(ms) {
     return (function* () {
       const start = Date.now()
       while (Date.now() - start < ms) yield
     })()
   },
-
   setTimeout(fn, delay, ...params) {
-    const sleep = this.sleep
-    const gen = (function* () {
+    const sleep = PM.run("AsyncTiming").sleep
+    return TS.add((function* () {
       yield* sleep(delay)
       yield* TS.run(fn, ...params)
-    })()
-    return TS.add(gen)
+    })())
   },
-
   setInterval(fn, delay, ...params) {
-    const sleep = this.sleep
-    const gen = (function* () {
+    const sleep = PM.run("AsyncTiming").sleep
+    return TS.add((function* () {
       while (true) {
         yield* sleep(delay)
         yield* TS.run(fn, ...params)
       }
-    })()
-    return TS.add(gen)
+    })())
   },
-
-  clearTimeout(id) {
-    TS.del(id)
-  },
-
-  clearInterval(id) {
-    TS.del(id)
-  }
+  clearTimeout(id)  { TS.del(id) },
+  clearInterval(id) { TS.del(id) }
 })
